7c4e6f794a44fc26cb7cf9d79a19521e64e965cb
Amer Wafiy <86043171+amerwafiy@users.noreply.github.com>
2022-05-07 20:20:28 +0800

    remove May 22 data

--- vacc-plot.py
+++ vacc-plot.py
@@ -7,6 +7,7 @@ plt.rcParams.update({'font.size': 14})
 df = pd.read_csv('vax_malaysia.csv')
 df = pd.DataFrame(df.groupby(pd.PeriodIndex(df['date'], freq="M"))['daily'].sum().reset_index())
 df['date'] = df['date'].astype(str)
+df = df[:-1]
 
 fig, ax = plt.subplots()
 x = ['Feb 21', 'Mac 21', 'Apr 21', 'May 21', 'Jun 21', 'Jul 21', 'Aug 21', 'Sep 21', 'Oct 21', 'Nov 21',
